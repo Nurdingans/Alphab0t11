c9dc6e2328492d2a2fe705bce14824af39d9b9e1
Nurdingans <96080159+Nurdingans@users.noreply.github.com>
2022-01-16 21:18:51 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -5,13 +5,13 @@ return `DONASI VIA E-MONEY
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0857 9832 8466
+	ㅁ 
 2. OVO
-	ㅁ 0857 9832 8466
+	ㅁ 0859 5047 1442
 3. DANA
 	ㅁ 0857 9832 8466
 	
-Semuanya atas nama NU******NS
+Semuanya atas nama TZ**IN
 `
 	}
 
